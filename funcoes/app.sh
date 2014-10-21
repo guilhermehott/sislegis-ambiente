988f59e295caebb24f4d5fd542e7e19d9aea0a8c
Paulo Jerônimo <paulojeronimo@gmail.com>
2014-10-21 11:37:13 -0200

    adição de funções a app.sh

--- funcoes/app.sh
+++ funcoes/app.sh
@@ -5,4 +5,8 @@ app_site() { browse http://github.com/pensandoodireito/sislegis-app; }
 app_baixar() { baixar-projeto app; }
 app_remover() { projetos && rm -rf app; }
 app() { projetos && cd app; }
+app_remote_add_upstream() { app && git remote add upstream http://github.com/pensandoodireito/sislegis-app; }
+app_fetch_upstream() { app && git fetch upstream; }
+app_merge_upstream_master() { app && git merge upstream/master; }
 app_pull() { app && git pull; }
+app_clean_package() { app && mvn clean package "$@"; }
